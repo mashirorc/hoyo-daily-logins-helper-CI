f06eb4273e73820fd85edf7c2ac859d0f716abc2
mashirorc <randychen401@gmail.com>
2023-07-21 16:02:49 -0700

    Update send.py

--- send.py
+++ send.py
@@ -3,13 +3,12 @@ import os
 from onepush import get_notifier
 
 WEBHOOK = os.environ['WEBHOOK']
-with open('output.log', 'r', encoding='utf-8') as f:
-    log_content = f.read()
+log_content = f.read()
 
 
-    #if "Message: OK" not in log_content:
-    n = get_notifier('discord')
-    print(n.params)  
+#if "Message: OK" not in log_content:
+n = get_notifier('discord')
+print(n.params)  
     
-    response = n.notify(webhook=WEBHOOK,content=log_content)
-    print(response.text) 
+response = n.notify(webhook=WEBHOOK,content=log_content)
+print(response.text) 
